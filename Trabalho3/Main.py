425901f968d6d9786c6fd11d4ce0711fc2987107
Natsu Dragneel <lagaleno@natsu.local>
2018-09-06 23:40:15 -0300

    + corpo do algoritmo; ERRO: lista

--- Trabalho3/Main.py
+++ Trabalho3/Main.py
@@ -14,7 +14,7 @@ pos_x = 0.0
 pos_y = 0.0
 
 conj_pontos = [] #Representa o conjunto de pontos da minha entrada que quero encontrar o Fecho Convexo
-
+aux = []
 pontos = [] #Lista de pontos do meu fecho convexo
 
 def main():
@@ -46,6 +46,14 @@ def display():
 
     for ponto in conj_pontos:
         glVertex2f(ponto[0], ponto[1]) #Desenhando os pontos ao clicar na posição
+
+    glEnd()
+
+
+    glBegin(GL_LINE_STRIP)
+
+    for p in pontos:
+        glVertex2f(p[0], p[1]) #Desenhando as linhas entre os pontos do Fecho Convexo
 
     glEnd()
 
@@ -88,40 +96,58 @@ def keyboard(key, x, y):
 
 def jarvis():
 
-    global pontos
+    global pontos, aux
 
     #p0 representa o ponto inicial de menor coordenada y
+
     conj_pontos.sort(key=lambda k: k[1]) #Ordeando de maneira crescente a lisa de entrada deconjunto depontos
+    aux = conj_pontos.copy()
+
     pontos.append(conj_pontos[0]) #Com isso p0 terá o vértice com o menor y! Que está na primeira posição do conj_pontos
-    pontos.append(proximo(pontos[0], [1, 0])) #pegando o p2
+    v0_x = pontos[0][0] - (pontos[0][0] + 100)
+    v0_y = pontos[0][1]
+    aux.remove(pontos[0])
+    pontos.append(proximo(pontos[0], v0_x, v0_y)) #pegando o p2
+    prox = pontos[1]
 
-    print(pontos)
     i = 1
+    while prox != pontos[0]:
 
-    while pontos[i] != pontos[0]:
+        #vetor = [pontos[i][0] - pontos[i-1][0], pontos[i][1] - pontos[i-1][1]]
 
-        pontos[i].append(proximo(pontos[i-1], ))
+        v1_x = pontos[i][0] - pontos[i-1][0]
+        v1_y = pontos[i][1] - pontos[i-1][1]
+        aux.remove(pontos[i])
+
+        prox = proximo(pontos[i], v1_x, v1_y)
+
+        pontos.append(prox)
         i = i+1
 
+        print(pontos)
 
 '''
     O retorno da função é o ponto que possui a menor angulação com o vetor.
 '''
-def proximo(ponto, vetor):
+def proximo(ponto, v1_x, v1_y):
 
     menor = [] #A variavel menor irá guardar o ponto que possui a angulação menor
     cos_ant = -2
 
     #Retorna um ponto p tal que o angulo de v e p0p seja minimo
 
-    menor = ponto
+    for i in range(0, len(aux)):
+        #vetor2 = [ponto[0] - aux[i][0], ponto[1] - aux[i][1]]
 
-    for p in conj_pontos:
-        cos = angulo(vetor, )
+        v2_x = ponto[0] - aux[i][0]
+        v2_y = ponto[1] - aux[i][1]
 
+        cos = angulo(v1_x, v1_y, v2_x, v2_y)
         if cos > cos_ant:
-            #muda menor
-            pass
+            menor = []
+            menor.append(aux[i])
+
+    return menor
 
 '''
     Para calcular o angulo, iremos usar o Produto interno para achar o cos(teta);
@@ -131,18 +157,17 @@ def proximo(ponto, vetor):
 
     O retorno da função é cos(teta) entre o vetor que sai do ponto e o vetor que liga os pontos.
 '''
-def angulo(v1, v2):
+def angulo(v1_x, v1_y, v2_x, v2_y):
 
     #Fórmula do Produto interno
     # cos(teta) = c = <v1,v2> / |v1||v2|
 
-    norma_v1 = math.sqrt(v1[0]**2 + v1[1]**2)
+    norma_v1 = math.sqrt(v1_x**2 + v1_y**2)
 
-    norma_v2 = math.sqrt(v2[0]**2 + v2[1]**2)
+    norma_v2 = math.sqrt(v2_x**2 + v2_y**2)
 
-    cos = ((v1[0] * v2[0]) + (v1[1] * v2[1]))/(norma_v1 * norma_v2)
+    cos = ((v1_x * v2_x) + (v1_y * v2_y))/(norma_v1 * norma_v2)
 
-    print(cos)
 
     return cos
 
